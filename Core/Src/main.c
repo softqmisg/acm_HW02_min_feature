d901fd28c1f9431204ad55722417c5777329ccc2
Mehdi Bagherimofidi <mehdi.bagherimofidi@yahoo.com>
2023-04-11 15:31:02 +0330

    Final noESP32

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -43,7 +43,7 @@
 #include "tmp275.h"
 #include "fonts/font_tahoma.h"
 #include "fonts/font_verdana.h"
-//#include "images/acm_logo.h"
+#include "images/acm_logo.h"
 #include "graphics.h"
 #include "st7565.h"
 #include "libbmp.h"
@@ -388,7 +388,6 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 //	}
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 uint8_t uart_transmit_frame(char *msg, cmd_type_t cmd_type, uint8_t cmd_code) {
 	uint8_t transmit_frame[120];
 	transmit_frame[0] = 0xaa;
@@ -2367,48 +2366,46 @@ void split_date(char *str, RTC_DateTypeDef *d) {
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int app_main(void) {
-	/* USER CODE BEGIN 1 */
+  * @brief  The application entry point.
+  * @retval int
+  */
+int app_main(void)
+{
+  /* USER CODE BEGIN 1 */
 
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_DMA_Init();
-	MX_I2C3_Init();
-	MX_TIM4_Init();
-	MX_RTC_Init();
-	MX_SDIO_SD_Init();
-	MX_FATFS_Init();
-	MX_USB_DEVICE_Init();
-	MX_USART2_UART_Init();
-	MX_USART3_UART_Init();
-	MX_USB_HOST_Init();
-	MX_LWIP_Init();
-
-#if !(__DEBUG__)
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_I2C3_Init();
+  MX_TIM4_Init();
+  MX_RTC_Init();
+  MX_SDIO_SD_Init();
+  MX_FATFS_Init();
+  MX_USB_DEVICE_Init();
+  MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
+  MX_USB_HOST_Init();
+  MX_LWIP_Init();
   MX_IWDG_Init();
-#endif
-	/* USER CODE BEGIN 2 */
+  /* USER CODE BEGIN 2 */
 	/////////////////////////
 	uint8_t flag_change_form = 0;
 	uint8_t counter_change_form = 0;
@@ -2522,6 +2519,7 @@ int app_main(void) {
 	HAL_IWDG_Refresh(&hiwdg);
 #endif
 
+
 	if (BSP_SD_IsDetected() == SD_PRESENT) {
 		HAL_Delay(100);
 		if (BSP_SD_IsDetected() == SD_PRESENT) {
@@ -2553,7 +2551,6 @@ int app_main(void) {
 		SDFatFS.fs_type = 0;
 		HAL_SD_DeInit(&hsd);
 	}
-
 	glcd_blank();
 
 	///////////////////////initialize & checking sensors///////////////////////////////////////
@@ -2878,6 +2875,7 @@ int app_main(void) {
 			prev_doorstate = cur_doorstate;
 		}
 		//////////////////////////Saves parameters in logs file in SDCARD,just one  time////
+
 		if (flag_log_param /*&& USBH_MSC_IsReady(&hUsbHostHS)*/) {
 			flag_log_param = 0;
 			sprintf(tmp_str2,
@@ -2966,6 +2964,7 @@ int app_main(void) {
 			r_logparam = Log_file(SDCARD_DRIVE, PARAMETER_FILE, tmp_str2);
 
 		}
+
 		/////////////////////////////////////State Machine///////////////////////////////////////////////////////////
 		switch (MENU_state) {
 		/////////////////////////////////////MAIN_MENU/////////////////////////////////////////////////
@@ -3216,6 +3215,7 @@ int app_main(void) {
 			}
 			break;
 			/////////////////////////////////////PASS_MENU/////////////////////////////////////////////////
+
 		case PASS_MENU:
 			joystick_init(Key_DOWN | Key_TOP | Key_LEFT | Key_RIGHT | Key_ENTER,
 					Long_press);
@@ -9208,7 +9208,9 @@ int app_main(void) {
 			DISP_state = DISP_IDLE;
 			flag_change_form = 1;
 			break;
+
 		}
+
 		///////////////////////////////////////////////END SWITCH//////////////////////////////////////////////////////
 		/////////////////////Temperature Control Algorithm//////////////////////////////////
 		if (TEC_STATE_Value) {
@@ -10213,6 +10215,7 @@ int app_main(void) {
 
 			}
 		}
+
 		///////////send i am ready to ESP32///////////////////
 		if(!ESP32ready && sendready_tick)
 		{
@@ -10228,8 +10231,7 @@ int app_main(void) {
 			uart_transmit_frame("", cmd_error, UploadError);
 			printf("upload started & frame error\n\r");
 		}
-		///////////////////////////////////////////////////////////////////////////////////////////////////
-
+		///////////////////////////////////////////end ESP32////////////////////////////////////////////////////////
 #if __LWIP__
 		MX_LWIP_Process();
 #endif
@@ -10273,103 +10275,113 @@ int app_main(void) {
 	HAL_IWDG_Refresh(&hiwdg);
 #endif
 	}
-	/* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1) {
-		/* USER CODE END WHILE */
-		MX_USB_HOST_Process();
+#if 0
 
-		/* USER CODE BEGIN 3 */
+    /* USER CODE END WHILE */
+    MX_USB_HOST_Process();
+
+    /* USER CODE BEGIN 3 */
+#endif
+
 	}
-	/* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
-	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = { 0 };
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
-	/** Configure the main internal regulator output voltage
-	 */
-	__HAL_RCC_PWR_CLK_ENABLE();
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI
-			| RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-	RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 25;
-	RCC_OscInitStruct.PLL.PLLN = 336;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = 7;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE
+                              |RCC_OSCILLATORTYPE_LSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
-		Error_Handler();
-	}
-	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
-	PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
-	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
+  PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
 
-/**
- * @brief  Period elapsed callback in non blocking mode
- * @note   This function is called  when TIM1 interrupt took place, inside
- * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
- * a global variable "uwTick" used as application time base.
- * @param  htim : TIM handle
- * @retval None
- */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-	/* USER CODE BEGIN Callback 0 */
+ /**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
 
-	/* USER CODE END Callback 0 */
-	if (htim->Instance == TIM1) {
-		HAL_IncTick();
-	}
-	/* USER CODE BEGIN Callback 1 */
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
 	if (htim->Instance == TIM1 && joystick_state() == jostick_initialized) {
 		joystick_read(Key_ALL, no_press);
 	}
-	/* USER CODE END Callback 1 */
+  /* USER CODE END Callback 1 */
 }
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
